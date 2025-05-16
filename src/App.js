5b81edcb22f5e29d83f884a7c880ad9dbf4014a9
smilligan-sm <sarahlouisemilligan.sm@gmail.com>
2025-05-17 09:10:16 +1000

    Added CSS styling

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
       <Weather/>
-      <br></br>
-      <br></br>
       <footer>
       Built by <a href="https://github.com/smilligan-sm" target="_blank" rel="noreferrer">Sarah Milligan</a>, is open-sourced on <a href="https://github.com/smilligan-sm/sm-react-weather-app" target="_blank" rel="noreferrer">Github</a
       > and hosted on <a href="https://tiny-concha-66febd.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
--- src/Weather.js
+++ src/Weather.js
@@ -4,27 +4,32 @@ import "./Weather.css";
 export default function Weather() {
     return <div className="Weather">
         <form>
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-9">
-                    <input type="search" placeholder="Enter a city..." className="form-control"/>
+                    <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on"/>
                 </div>
                 <div className="col-3">
-                    <input type="submit" value="Search" className="btn btn-primary"/>
+                    <input type="submit" value="Search" className="btn btn-primary w-100"/>
                 </div>
             </div>
         </form>
         <h1>Canberra</h1>
-        <p>Friday 15:00</p>
-        <p>Mostly Cloudy</p>
+        <ul>
+        <li>Friday 15:00</li>
+        <li>Mostly Cloudy</li>
+        </ul>
         <div className="row">
             <div className="col-6">
                 <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy"/>
-                12°C
+                <span className="temperature">12</span>
+                <span className="unit">°C</span>
             </div>
             <div className="col-6">
-                <p>Percipitation: 15%</p>
-                <p>Humidity: 72%</p>
-                <p>Wind Speed: 13 km/h</p>
+                <ul>
+                <li>Percipitation: 15%</li>
+                <li>Humidity: 72%</li>
+                <li>Wind Speed: 13 km/h</li>
+                </ul>
             </div>
         </div>
         </div>;
