b704c535ff2ecca00bb219679a1b0ffb45574eae
smilligan-sm <sarahlouisemilligan.sm@gmail.com>
2025-05-16 15:04:25 +1000

    Added footer with GitHub and Netfify links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>Weather App</h1>
+      <br></br>
+      <br></br>
+      <footer>
+      Built by <a href="https://github.com/smilligan-sm" target="_blank" rel="noreferrer">Sarah Milligan</a>, is open-sourced on <a href="https://github.com/smilligan-sm/sm-react-weather-app" target="_blank" rel="noreferrer">Github</a
+      > and hosted on <a href="https://tiny-concha-66febd.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
+    </footer>
+    </div>
+    
   );
 }
